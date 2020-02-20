e7cac60962a8ea69a3de4d41c4324496d3e34ab6
Erin Knotek <erinknotek@60-30-d4-5e-25-52.wireless.temple.edu>
2020-02-20 14:33:05 -0500

    Updated Changes before R practice course

--- Data_Cleaning.R
+++ Data_Cleaning.R
@@ -6,4 +6,6 @@ library(dplyr)
 # load dataset
 Screentime_Data <- read_excel("~/Desktop/ScreentimeData.xlsx")
 
+Screentime_Data %>% 
+  filter(ScreenTime_Proxy_HrsPerDay!=0) %>% mean(.$ScreenTime_Proxy_HrsPerDay)
 
